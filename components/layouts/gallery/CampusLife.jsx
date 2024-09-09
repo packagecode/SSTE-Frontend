ef8d043718db73ebbd8869b23220f893244bd480
sihab <sihab@sokrio.com>
2024-09-09 11:14:30 +0600

    modify campus life modal

--- components/layouts/gallery/CampusLife.jsx
+++ components/layouts/gallery/CampusLife.jsx
@@ -6,22 +6,94 @@ import Carousel from "react-multi-carousel";
 import Image from "next/image";
 
 const ShowImageModal = ({ image, closeModal, caption }) => {
+  const openLink = () => {
+    window.open(image, "_blank");
+  };
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[999999]">
-      <div className="bg-white p-10 rounded-lg relative flex  flex-col items-center gap-y-4">
-        <button
-          onClick={closeModal}
-          className="absolute top-0 right-0 p-3 h-8 w-8 flex items-center justify-center m-2 bg-red-500 text-white rounded-full"
-        >
-          X
-        </button>
-        <Image
-          className="h-96 w-full object-cover object-center"
-          src={image || ''}
-          alt="Image" layout="responsive"
-          width={100} height={96}
-        />
-        <div className="">{caption}</div>
+    // <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[999999]">
+    //   <div className="bg-white p-10 rounded-lg relative flex  flex-col items-center gap-y-4">
+    //     <button
+    //       onClick={closeModal}
+    //       className="absolute top-0 right-0 p-3 h-8 w-8 flex items-center justify-center m-2 bg-red-500 text-white rounded-full"
+    //     >
+    //       X
+    //     </button>
+    //     <Image
+    //       className="h-96 w-full object-cover object-center"
+    //       src={image || ''}
+    //       alt="Image" layout="responsive"
+    //       width={100} height={96}
+    //     />
+    //     <div className="">{caption}</div>
+    //   </div>
+    // </div>
+    <div
+      class="relative z-10 z-[999999]"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      {/*   
+    Background backdrop, show/hide based on modal state.
+
+    Entering: "ease-out duration-300"
+      From: "opacity-0"
+      To: "opacity-100"
+    Leaving: "ease-in duration-200"
+      From: "opacity-100"
+      To: "opacity-0" */}
+
+      <div
+        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+        aria-hidden="true"
+      ></div>
+
+      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          {/*       
+        Modal panel, show/hide based on modal state.
+
+        Entering: "ease-out duration-300"
+          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          To: "opacity-100 translate-y-0 sm:scale-100"
+        Leaving: "ease-in duration-200"
+          From: "opacity-100 translate-y-0 sm:scale-100"
+          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+       */}
+          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <button
+              onClick={closeModal}
+              className="absolute top-0 right-0 p-3 h-8 w-8 flex items-center justify-center m-2 bg-red-500 text-white rounded-full"
+            >
+              X
+            </button>
+            <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 clear-both mt-10">
+              <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                <Image
+                  className="h-96 w-full object-cover object-center hover:cursor-pointer"
+                  src={image || ""}
+                  alt="Image"
+                  layout="responsive"
+                  width={100}
+                  height={96}
+                  onClick={openLink}
+                />
+                <div class="mt-2">
+                  <p class="text-lg text-gray-500">{caption}</p>
+                </div>
+              </div>
+            </div>
+            <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button
+                type="button"
+                onClick={closeModal}
+                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -108,9 +180,10 @@ const CampusLife = () => {
             >
               <Image
                 className="h-48 max-w-full rounded-lg object-cover object-center hover:scale-110 transition-all duration-200"
-                src={image.image || ''}
+                src={image.image || ""}
                 alt="Image"
-                width={100} height={48}
+                width={100}
+                height={48}
                 layout="responsive"
               />
             </div>
